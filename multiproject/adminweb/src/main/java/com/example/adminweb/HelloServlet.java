da52eebae93f9fb4bd0612bbc7e21390f6643bca
Tsunenobu Kai <kai2nenobu@gmail.com>
2016-12-30 01:01:08 +0900

    adminwebにも適当なサーブレットを追加する

---
+++ multiproject/adminweb/src/main/java/com/example/adminweb/HelloServlet.java
@@ -0,0 +1,40 @@
+package com.example.adminweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.Greeting;
+
+@WebServlet(name="HelloServlet", urlPatterns={"/hello"})
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+		res.setContentType("text/html");
+
+		String user = req.getParameter("user");
+		Greeting greeting = new Greeting();
+		String message = htmlEscape(greeting.greet(user));
+
+        PrintWriter writer = res.getWriter();
+        writer.println("<!DOCTYPE html><html><body>");
+        writer.print("<h1>This is Admin Application</h1>");
+        writer.print("<p>" + message + "</p>");
+        writer.println("</body></html>");
+    }
+
+	private String htmlEscape(String input) {
+		if (input == null) return null;
+		return input.replace("&", "&amp;").replace("\"", "&quot;").replace("<", "&lt;").replace(">", "&gt;").replace("'", "&#39;");
+	}
+
+}
